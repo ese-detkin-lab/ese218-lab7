2ad1e7467150336acf35e327bafb1bf12329929b
Kim Luong <kimnguyenluong@gmail.com>
2021-11-11 09:34:24 -0500

    Cleaned up code with comments.

--- ese218_lab6_7/ese218_lab6_7.ino
+++ ese218_lab6_7/ese218_lab6_7.ino
@@ -1,70 +1,62 @@
 /***************************************************************************
-  This is a library for the BMP3XX temperature & pressure sensor. Designed specifically to work with the Adafruit BMP388 Breakout ----> http://www.adafruit.com/products/3966
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required to interface. Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit!
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries. BSD license, all text above must be included in any redistribution
+ * Creator: Kim Luong, 2021-11-10
+  All instructions are on the github repo. https://github.com/ese-detkin-lab/ese219-lab7
  ***************************************************************************/
-
-// Complete project details: https://RandomNerdTutorials.com/esp8266-nodemcu-bmp388-arduino/
 
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
 
+// If using SPI, replace the pin numbers appropriately
 #define BMP_SCK 14
 #define BMP_MISO 12
 #define BMP_MOSI 13
 #define BMP_CS 15
 
-#define BLYNK_PRINT Serial /* Comment this out to disable prints and save space */
-#define BLYNK_TEMPLATE_ID "TMPLBgeMqc0l"
-#define BLYNK_DEVICE_NAME "Kim test 2"
-#define BLYNK_AUTH_TOKEN "LXaBN8aRKf9d1aRnBcxTotufpLOlXduK"
+#define BLYNK_PRINT Serial 
 
-
+//************************************************************
+// REPLACE THE 3 LINES BELOW WITH YOUR OWN TEMPLATE INFORMATION
+#define BLYNK_TEMPLATE_ID "TemplateID"
+#define BLYNK_DEVICE_NAME "devicename"
+#define BLYNK_AUTH_TOKEN "somethingsecrethere"
+//***********************************************************
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
 char auth[] = BLYNK_AUTH_TOKEN;
 
 // Your WiFi credentials.
-// Set password to "" for open networks.
 char ssid[] = "AirPennNet-Device";
 char pass[] = "penn1740wifi";
 
+// Variables
 float t;
 float p;
 float a;
-
 int start_timer = 0;
 int start_time = 0;
 int saved_time = 0;
 int reset_timer = 0;
-
 Adafruit_BMP3XX bmp;
 BlynkTimer timer;
 
 // This function is called every time the device is connected to the Blynk.Cloud
 BLYNK_CONNECTED()
 {
-  // Change Web Link Button message to "Congratulations!"
- // Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
- // Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
-  //Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
 }
 
 void sendSensor()
 {
 
-   if (! bmp.performReading()) {
-    Serial.println("Failed to perform reading :(");
-    return;
+  if (! bmp.performReading()) 
+  {
+  Serial.println("Failed to perform reading :(");
+  return;
   }
   t = bmp.temperature;
   p = bmp.pressure / 100.0;  // in hPa
@@ -79,6 +71,8 @@ void sendSensor()
     saved_time = 0;
     reset_timer = 0;
   }
+
+  // Uncomment the following if you wish to print to serial monitor.
  /*  
   Serial.print("Temperature = ");
   Serial.print(t);
@@ -94,6 +88,8 @@ void sendSensor()
 
   Serial.println();
  */ 
+  
+  // Send information to blynk app
   Blynk.virtualWrite(V0, p); //V0 
   Blynk.virtualWrite(V1, t); //V1 
   Blynk.virtualWrite(V2,a); //V2
@@ -104,15 +100,15 @@ void setup() {
  // Serial.begin(115200);
  Serial.begin(9600);
   while (!Serial);
-   Serial.println(" ");
+  Serial.println(" ");
   Serial.println("Adafruit BMP388 / BMP390 test");
- Blynk.begin(auth, ssid, pass);
- Serial.println("BLYNK Start");
+  Blynk.begin(auth, ssid, pass);
+  Serial.println("BLYNK Start");
   if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
   //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
   //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
-    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
-    while (1);
+  Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+  while (1);
   }
 
   // Set up oversampling and filter initialization
@@ -145,10 +141,8 @@ BLYNK_WRITE(V4) {
 BLYNK_WRITE(V5) {
   Blynk.virtualWrite(V3,0);
   reset_timer = 1;
-//  int up = param.asInt();
-//  digitalWrite(16, up);
 }
 void loop() {
-   Blynk.run();
+  Blynk.run();
   timer.run(); // Initiates SimpleTimer
 }
